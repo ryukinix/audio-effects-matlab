15875fe4dea437fee6be4cd2763a061ff66f330d
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-09 09:04:07 -0300

    Traduz comentários do arquivo Flanger.m

--- src/Flanger.m
+++ src/Flanger.m
@@ -3,8 +3,8 @@ function [out] = Flanger(in,mix,delay,width,rate,fs)
 %
 % VARIÁVEIS:
 %   IN - vetor coluna de entrada representando um audio
-%   MIX - depth - amplitude do sinal atrasado adicionado em input (0 a 1)
-%   DELAY - tempo de delay mínimo - 100usec to 10msec (in msec) 0.1 to 10
+%   MIX - depth - amplitude do sinal atrasado adicionado na entrada (0 a 1)
+%   DELAY - tempo de delay mínimo - 100usec a 10msec (em msec) 0.1 to 10
 %   WIDTH - sweep depth - quão largo a troca é (100nsec to 10msec) 
 %           (in msec, 0.0001)
 %   RATE - frequência do Oscilador de Baixa Frequência (LFO) - 0.05 to 5 Hz
@@ -13,25 +13,26 @@ function [out] = Flanger(in,mix,delay,width,rate,fs)
     in=interp1(1:length(in),in,1:.25:length(in));
     fsn=fs*4;
 
-    minDelaySamp=ceil(delay*fsn/1000); %convert to msec, then samples
-    maxDelaySamp=ceil((delay+width)*fsn/1000); %convert to msec, then samples
-    n=(1:length(in)+maxDelaySamp)'; %how long to extend in by for LFO
-    LFO=sawtooth(2*pi*rate/(fsn)*n,.5); %sawtooth more commonly used in flangers
+    minDelaySamp=ceil(delay*fsn/1000); %converte para (ms), então amostras
+    maxDelaySamp=ceil((delay+width)*fsn/1000); %converte para (ms),então amostras
+    n=(1:length(in)+maxDelaySamp)'; %quanto extender pelo LFO 
+    LFO=sawtooth(2*pi*rate/(fsn)*n,.5); %sawtooth comumente usado
+                                        %em flangers
     delayTimeSamples=(delay+width/2+width/2*LFO)*fsn/1000; 
     % instantaneous delay in samples (computed by looking at graph from class
     % PDF)
 
-    out=zeros(length(in)+minDelaySamp,1); %initialized output vec
+    out=zeros(length(in)+minDelaySamp,1); % inicializar vetor de saída
 
     out(1:maxDelaySamp)=in(1:maxDelaySamp); 
-    % copy front of signal before min delay
+    % copiar frente do sinal antes do delay mínimo
 
-    for i=maxDelaySamp+1:length(in) % starting from next sample
-        delaySamples=ceil(delayTimeSamples(i)); %whole number of current delay
-        out(i)=in(i)+mix*out(i-delaySamples); %add input and fraction of delay 
+    for i=maxDelaySamp+1:length(in) % começando pela próxima amostra
+        delaySamples=ceil(delayTimeSamples(i)); %número inteiro do delay atual
+        out(i)=in(i)+mix*out(i-delaySamples); %adiciona entrada e fração de delay
     end
  
-    % Reduzir a amostragem por multiplicador 4
+   
     out=downsample(out,4);
 
 end
