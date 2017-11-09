4b6a6fc2a31a37034333b550567d36606bf7251b
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-09 09:07:26 -0300

    Melhora as descrições contidas em Distortion.m

--- src/Distortion.m
+++ src/Distortion.m
@@ -4,22 +4,22 @@ function [ y ] = Distortion(in, gain, tone, fs)
 % VARIÁVEIS:
 %   IN - audio de entrada como um vetor coluna input guitar audio column vector
 %   GAIN - quantidade de ganho ao aplicar o filtro -- (0 a 11)
-%   TONE - 0 para tons baixo, 1 para agudos (um polo no filtro passa banda)
+%   TONE - -1 para tons baixo, 1 para agudos (um polo no filtro passa banda)
 %     FS - Frequência de Amostragem
 
     dbstop if error
 
     % GAIN BEFORE NONLINEARITY
     b=in;
-    b=b/max(abs(b));
-    b=gain*b;
+    b=b/max(abs(b)); % normalização do sinal valor absoluto máximo
+    b=gain*b; % ganho
 
-    % NONLINEARITY
+    % Aplicar não-linearidade
     i=(b>=0.75); b(i)=1;  % hard clipping on +ve edge
     i=(b<=-2/3); b(i)=-1; % hard clipping on -ve edge
 
     % região linear deixada de lado entre -1/3 e 1/2
-    % parabola para conectar regi�es lineares e saturadas
+    % parabola para conectar regiões lineares e saturadas
 
     % coeficientes de parabola de corte suave no limite de +ve
     c1=4*sqrt(2); c2=3*sqrt(2);
@@ -34,8 +34,8 @@ function [ y ] = Distortion(in, gain, tone, fs)
     % TONE SECTION - sobrepor dois filtros butterworth de primeira ordem
     [ZL,PL,KL]= butter(1, 1200/(fs/2), 'low');
     [ZH,PH,KH]= butter(1, 265/(fs/2), 'high');
-    [BL,AL]=zp2tf(ZL,PL,KL); % calcula a fun��o de transfer�ncia
-    [BH,AH]=zp2tf(ZH,PH,KH); % fun��o de transfer�ncia
+    [BL,AL]=zp2tf(ZL,PL,KL); % calcula a função de transferência
+    [BH,AH]=zp2tf(ZH,PH,KH); % funçãoo de transferência
 
     y=(1-tone)*filter(BL,AL,b)+tone*filter(BH,AH,b);
 end
